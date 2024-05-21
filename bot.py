4e98e7a80c1d89ecc23cca5c2aaa80fc8072b901
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-21 12:16:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,12 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-import time
-import string
-import random
-import asyncio
-import aiofiles
-import datetime
-from broadcast_helper import send_msg
-from database import Database
-
-from pyrogram.types import Message
-db = Database("mongodb+srv://ft:Q5W6QD4HY1QGh5JN@cluster0.c1zu1o3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0","publicstore")
-broadcast_ids = {}
-
-
-
-
 # Configs
 API_HASH = os.environ['API_HASH']
 APP_ID = int(os.environ['APP_ID'])
 BOT_TOKEN = os.environ['BOT_TOKEN']
 TRACK_CHANNEL = int(os.environ['TRACK_CHANNEL'])
 OWNER_ID = os.environ['OWNER_ID']
-#DATABASE_URL = str(os.environ['DATABASE_URL',"mongodb+srv://ft:Q5W6QD4HY1QGh5JN@cluster0.c1zu1o3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"])
-
 #Button
 START_BUTTONS=[
     [
@@ -34,25 +15,23 @@ START_BUTTONS=[
     ],
     [InlineKeyboardButton('𝙲𝚑𝚊𝚗𝚗𝚎𝚕𝚜🚨', url="https://t.me/+ExBm8lEipxRkMTA1")],
 ]
-
 # Running bot
 xbot = Client('File-Sharing', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
 # Notify about bot start
 with xbot:
     xbot_username = xbot.get_me().username  # Better call it global once due to telegram flood id
     print("Bot started!")
     xbot.send_message(int(OWNER_ID), "Bot started!")
 
-
 # Start & Get file
 @xbot.on_message(filters.command('start') & filters.private)
 async def _startfile(bot, update):
     if update.text == '/start':
         await update.reply_text(
-            f"I'm File-Sharing!\nYou can share any telegram files and get the sharing link using this bot!\n\n<blockquote> If You Send Singlefile you get single link or if you send a multiple file you get multiple files single link limit upto 100</blockquote>\n\n/help for more details...",
+            f"I'm File-Sharing!\nYou can share any telegram files and get the sharing link using this bot!\n\n  If You Send Singlefile you get single link or if you send a multiple file you get multiple files single link limit upto 100 \n\n/help for more details...",
             True, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
         return
-
     if len(update.command) != 2:
         return
     code = update.command[1]
@@ -60,7 +39,6 @@ async def _startfile(bot, update):
         msg_id = code.split('-')[-1]
         # due to new type of file_unique_id, it can contain "-" sign like "agadyruaas-puuo"
         unique_id = '-'.join(code.split('-')[0:-1])
-
         if not msg_id.isdigit():
             return
         try:  # If message not belong to media group raise exception
@@ -68,7 +46,6 @@ async def _startfile(bot, update):
             check = check_media_group[0]  # Because func return`s list obj
         except Exception:
             check = await bot.get_messages(TRACK_CHANNEL, int(msg_id))
-
         if check.empty:
             await update.reply_text('Error: [Message does not exist]\n/help for more details...')
             return
@@ -96,8 +73,6 @@ async def _startfile(bot, update):
             await check.copy(update.from_user.id)
     else:
         return
-
-
 # Help msg
 @xbot.on_message(filters.command('help') & filters.private)
 async def _help(bot, update):
@@ -105,6 +80,7 @@ async def _help(bot, update):
 
 
 async def __reply(update, copied):
+    
     msg_id = copied.id
     if copied.video:
         unique_idx = copied.video.file_unique_id
@@ -125,17 +101,15 @@ async def __reply(update, copied):
     else:
         await copied.delete()
         return
-
     await update.reply_text(
-        "Here is Your Sharing Link 🔗: f'https://telegram.me/{xbot_username}?start={unique_idx.lower()}-{str(msg_id)}'",
+        'Here is Your Sharing Link:',
         True,
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton('🖇️Sharing Link🖇️',
+            [InlineKeyboardButton('Sharing Link',
                                   url=f'https://telegram.me/{xbot_username}?start={unique_idx.lower()}-{str(msg_id)}')]
         ])
     )
     await asyncio.sleep(0.5)  # Wait do to avoid 5 sec flood ban 
-
 # Store media_group
 media_group_id = 0
 @xbot.on_message(filters.media & filters.private & filters.media_group)
@@ -151,14 +125,13 @@ async def _main_grop(bot, update):
     if int(media_group_id) != int(update.media_group_id):
         media_group_id = update.media_group_id
         copied = (await bot.copy_media_group(TRACK_CHANNEL, update.from_user.id, update.id))[0]
+        copied = (await bot.copy_media_group(TRACK_CHANNEL, update.from_user.id, update.id))[0]
         await __reply(update, copied)
 
     else:
         # This handler catch EVERY message with [update.media_group_id] param
         # So we should ignore next >1_media_group_id messages
         return
-
-
 # Store file
 @xbot.on_message(filters.media & filters.private & ~filters.media_group)
 async def _main(bot, update):
@@ -168,81 +141,6 @@ async def _main(bot, update):
         pass
     else:
         return
-
     copied = await update.copy(TRACK_CHANNEL)
     await __reply(update, copied)
-
-@xbot.on_message(filters.command("users") & filters.private)
-async def sts(c: Client, m: Message):
-    user_id=m.from_user.id 
-    if 'user_id' in 'OWNER_ID':
-        total_users = await db.total_users_count()
-        await m.reply_text(text=f"Total Users in DB: {total_users}", quote=True)
-        
-        
-@xbot.on_message(filters.command("broadcast") & filters.private & filters.user(OWNER_ID))
-async def broadcast_(c, m):
-    user_id=m.from_user.id
-    out = await m.reply_text(
-            text=f"Broadcast initiated! You will be notified with log file when all the users are notified."
-    )
-    all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    broadcast_ids[broadcast_id] = dict(
-        total=total_users,
-        current=done,
-        failed=failed,
-        success=success
-    )
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(
-                user_id=int(user['id']),
-                message=broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user['id'])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current=done,
-                        failed=failed,
-                        success=success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    os.remove('broadcast.txt')
 xbot.run()
